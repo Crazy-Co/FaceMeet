0875bd809dc36497e98d92d34fe64908e7ca847f
Khushdev <khushdevyogi001@gmail.com>
2024-08-20 07:17:54 +0530

    First commit

---
+++ server/index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const PORT = 8000;
+
+const app = express();
+const server = new createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+const emailToSocketIdMap = new Map();
+const socketIdToEmailMap = new Map();
+
+io.on("connection", (socket) => {
+  console.log("Socket Connected", socket.id);
+
+  socket.on("room:join", (data) => {
+    console.log(data);
+    const { email, room } = data;
+    emailToSocketIdMap.set(email, socket.id);
+    socketIdToEmailMap.set(socket.id, email);
+
+    io.to(room).emit("user:joined", { email, id: socket.id });
+
+    socket.join(room);
+
+    io.to(socket.id).emit("room:join", data);
+  });
+
+  socket.on("user:call", ({ to, offer }) => {
+    io.to(to).emit("incomming:call", { from: socket.id, offer });
+  });
+
+  socket.on("call:accepted", ({ to, ans }) => {
+    io.to(to).emit("call:accepted", { from: socket.id, ans });
+  });
+
+  socket.on("peer:nego:needed", ({ to, offer }) => {
+    io.to(to).emit("peer:nego:needed", { from: socket.id, offer });
+  });
+
+  socket.on("peer:nego:done", ({ to, ans }) => {
+    io.to(to).emit("peer:nego:final", { from: socket.id, ans });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Socket Disconnected", socket.id);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
